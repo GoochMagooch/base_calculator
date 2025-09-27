fa2c8d1d87405ab4348e06dc877f06ca8bf052bc
Magooch <crvallecito@gmail.com>
2025-09-27 02:20:49 -0600

    Create script source file and add test function

---
+++ script.js
@@ -0,0 +1,6 @@
+let testBtn = document.getElementById("test");
+let testPara = document.getElementById("test-para");
+
+testBtn.addEventListener("click", function() {
+	testPara.textContent = "Test complete";
+})
