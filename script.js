47160e41cb13c05566afaa56acf39dace6be421e
Magooch <crvallecito@gmail.com>
2025-09-27 22:46:09 -0600

    Write conversion algorithm to convert decimal from base 2 - 9, depending on user choice

--- script.js
+++ script.js
@@ -1,35 +1,64 @@
 let input = document.getElementById("num-input")
 let convert = document.getElementById("convert")
 let output = document.getElementById("conversion-output")
+let dropdown = document.getElementById("choices")
 
 // Convert decimal to base 2
 convert.addEventListener("click", function() {
 	output.textContent = ""
 	let myNum = input.value
-	let binary = ""
+	let baseStr = ""
+	let base = 0
 
-	while (myNum > 1) {
-		if (myNum == 2) {
-			binary += 0
-			myNum = myNum / 2
+	// Choose base conversion
+	if (dropdown.value == "base-2") {
+		base = 2
+	} else if (dropdown.value == "base-3") {
+		base = 3
+	} else if (dropdown.value == "base-4") {
+		base = 4
+	} else if (dropdown.value == "base-5") {
+		base = 5
+	} else if (dropdown.value == "base-6") {
+		base = 6
+	} else if (dropdown.value == "base-7") {
+		base = 7
+	} else if (dropdown.value == "base-8") {
+		base = 8
+	} else if (dropdown.value == "base-9") {
+		base = 9
+	} 
+
+	// Convert decimal to chosen base form
+	while (myNum > base-1) {
+		if (myNum == base) {
+			baseStr += 0
+			myNum = myNum / base
 		} else {
-			if (myNum % 2 == 0) {
-				binary += 0
-				myNum = myNum / 2
+			if (myNum % base == 0) {
+				baseStr += 0
+				myNum = myNum / base
 			} else {
-				binary += 1
-				myNum = (myNum-1) / 2
+				baseStr += myNum % base
+				myNum = (myNum-myNum%base) / base
 			}
 		}
 	}
-	if (myNum == 1) {
-		binary += 1
+	if (myNum < base) {
+		baseStr += myNum
 	}
 
-	// Reverse base 2 string for output
+	// Reverse base string for output
 	let ans = ""
-	for (let i = binary.length-1; i >= 0; i--) {
-		output.textContent = input.value + " in binary: " + (ans += binary[i])
+	for (let i = baseStr.length-1; i >= 0; i--) {
+		ans += baseStr[i]
+	}
+
+	// Output depending on base conversion
+	if (dropdown.value == "base-2") {
+		output.textContent = input.value + " in binary: " + ans
+	} else {
+		output.textContent = input.value + " in base " + base + ": " + ans
 	}
 	input.value = ""
 })
